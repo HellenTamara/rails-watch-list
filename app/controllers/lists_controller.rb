96100cb114ba05316fc26bc03240344f4b6bc885
Hellen Tamara <hellen.tamara@outlook.com>
2024-02-16 11:07:16 +0900

    styles added and form fixed

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -6,7 +6,7 @@ class ListsController < ApplicationController
   def show
     @list = List.find(params[:id])
     @bookmark = Bookmark.new
-    @movies = @list.movies
+    @movies = Movie.where.not(id: @list.movies).order(:title)
   end
 
   def new
